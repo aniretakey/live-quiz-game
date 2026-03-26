001538536bbef895bfef1c22cae078162dcef3d1
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-26 21:01:17 +0300

    add clients to array on connection, add types

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,27 @@
-import {WebSocketServer} from 'ws';
+import WebSocket, {WebSocketServer} from 'ws';
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 // WebSocket server
 const wss = new WebSocketServer({port: PORT});
 
-wss.on('connection', (ws) => {
+const clients: WebSocket[] = []
+
+wss.on('connection', (ws: WebSocket) => {
     console.log('Connected!');
+    clients.push(ws);
+    console.log('clients', clients)
 
     ws.on('message', (msg) => {
-        console.log('Received message', msg);
-        ws.send('Message delivered!')
+        clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(msg);
+            }
+        });
     })
 
     ws.on('close', (ws) => {
-        console.log('Closed!')
+        console.log('Closed!, number: ', ws);
+        // clients.splice(clients.indexOf(ws), 1);
     });
 })
