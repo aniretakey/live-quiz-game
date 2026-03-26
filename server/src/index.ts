24d77ae931acb4b2693f9a27786450220fa2e038
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-26 20:38:27 +0300

    add server connection messages

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,19 @@
-import { WebSocketServer } from 'ws';
-
+import {WebSocketServer} from 'ws';
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 // WebSocket server
-const wss = new WebSocketServer({ port: PORT });
+const wss = new WebSocketServer({port: PORT});
+
+wss.on('connection', (ws) => {
+    console.log('Connected!');
+
+    ws.on('message', (msg) => {
+        console.log('Received message', msg);
+        ws.send('Message delivered!')
+    })
+
+    ws.on('close', (ws) => {
+        console.log('Closed!')
+    });
+})
