a2745ebee7f433ac9dcdaa44252bce70828ab71a
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-29 17:29:37 +0300

    add utils for create game

---
+++ server/src/utils/generateGameCodeAndId.ts
@@ -0,0 +1,19 @@
+import { randomUUID } from "node:crypto";
+
+function generateCode() {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < 6; i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+}
+
+export const generateGameCodeAndId = (): {
+    code: string, gameId: string
+} => {
+    const gameId = randomUUID();
+    const code = generateCode()
+
+    return {gameId, code}
+}
